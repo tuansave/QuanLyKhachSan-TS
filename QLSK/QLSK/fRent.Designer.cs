c8bb70609ab47f987fa0fe06b1a6eabe89314402
tuansave <16521371@gm.uit.edu.vn>
2019-07-08 23:33:06 +0700

    Revert "update"
    This reverts commit aaa388ddf1dd6f41405813f63c8b61017952032f.

--- QLSK/QLSK/fRent.Designer.cs
+++ QLSK/QLSK/fRent.Designer.cs
@@ -28,8 +28,9 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
             this.panel1 = new System.Windows.Forms.Panel();
+            this.txbRoomName = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.customerAddress = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.cbxFormality = new System.Windows.Forms.ComboBox();
@@ -58,11 +59,20 @@
             // panel1
             // 
             this.panel1.BackColor = System.Drawing.Color.Goldenrod;
+            this.panel1.Controls.Add(this.txbRoomName);
             this.panel1.Controls.Add(this.label1);
             this.panel1.Location = new System.Drawing.Point(0, -3);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(1052, 66);
             this.panel1.TabIndex = 1;
+            // 
+            // txbRoomName
+            // 
+            this.txbRoomName.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txbRoomName.Location = new System.Drawing.Point(93, 26);
+            this.txbRoomName.Name = "txbRoomName";
+            this.txbRoomName.Size = new System.Drawing.Size(132, 26);
+            this.txbRoomName.TabIndex = 5;
             // 
             // label1
             // 
@@ -161,14 +171,14 @@
             // 
             this.dtgvInputCustomes.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
             this.dtgvInputCustomes.BackgroundColor = System.Drawing.Color.DarkGray;
-            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
-            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Control;
-            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.WindowText;
-            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
-            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
-            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
-            this.dtgvInputCustomes.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dtgvInputCustomes.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle2;
             this.dtgvInputCustomes.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dtgvInputCustomes.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.customerName,
@@ -203,6 +213,7 @@
             this.cbxRoomCode.Name = "cbxRoomCode";
             this.cbxRoomCode.Size = new System.Drawing.Size(157, 28);
             this.cbxRoomCode.TabIndex = 0;
+           
             // 
             // panel4
             // 
@@ -320,5 +331,6 @@
         private DevExpress.XtraEditors.SimpleButton btnBack;
         private System.Windows.Forms.ComboBox cbxRoomCode;
         private DevExpress.XtraEditors.SimpleButton btnDelete;
+        private System.Windows.Forms.TextBox txbRoomName;
     }
 }
